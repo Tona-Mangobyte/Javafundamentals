1e74a4bd83732aefe9669057eebf4b6a07829e66
Tona-Mangobyte <tona@mango-byte.com>
2023-03-24 22:06:54 +0700

    add empy

---
+++ src/main/java/org/example/oop/Employee.java
@@ -0,0 +1,56 @@
+package org.example.oop;
+
+import java.time.LocalDate;
+
+public abstract class Employee {
+    private static final String DEFAULT_NAME = "UNKNOWN";
+    private static int nextId;
+
+    private Integer id;
+    private String name;
+    private LocalDate hireDate;
+
+    public Employee() {
+        this(DEFAULT_NAME);
+    }
+    public Employee(String name) {
+        id = nextId++;
+        this.name = name;
+        hireDate = LocalDate.now();
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getHireDate() {
+        return hireDate;
+    }
+
+    public void setHireDate(LocalDate hireDate) {
+        this.hireDate = hireDate;
+    }
+
+    public abstract double getPay();
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", hireDate=" + hireDate +
+                '}';
+    }
+}
